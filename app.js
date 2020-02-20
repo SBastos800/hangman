b1c3715e3efbb7de48ce217800957911316891f7
Sonia Bastos <Sonia.Bastos@Nology.local>
2020-02-20 08:41:52 +0000

    class success and error already working

--- app.js
+++ app.js
@@ -1,68 +1,94 @@
 const $onload = () => {
 
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    const maxTies = 10;  //maximum number of times to guess
+    let maxTries = 10;  //maximum number of times to guess
     let guessedLetters = [];  // Stores the letters the user guessed
     let guessingWord = [];    // This will be the word we actually build to match the current word
     let remainingGuesses = 10; // How many tries the player has left (10 or empty??)
-
     let lettersTypedByWordCreator = ""; //Stores the letters typed by word creator
 
     let input = document.getElementById('word');
-    let arrayOflettersTypedByCreator = [];
-    let letterTyped = "";
+    let arrayOfLettersTypedByCreator = [];
+    // let letterTyped = "";
+    
+    const pushIndexOfLetters = (item) => {
+        let indexOfEqualLetters = [];
+        for (let i = 0; i < lettersTypedByWordCreator.length; i++) {
 
-   
+            if (item.html() === lettersTypedByWordCreator[i]) indexOfEqualLetters.push(i)
+            
+        }
+        return indexOfEqualLetters;
+    }
+
+    const giveLetterAColor = (item) => {
+        if (arrayOfLettersTypedByCreator.includes(item.html())) {
+            item.addClass('success');
+            const indexes = pushIndexOfLetters(item);
+            // select all .underscore divs
+            // replace the .text with the letter at indexes
+            // console.log(arrayOfLettersTypedByCreator.indexOf(item.html()));
+
+            // add to word array
+        } else {
+            item.addClass('error');
+            maxTries--;
+            $('.lives').text(`You have ${maxTries} lives left`)
+        };
+    }
 
     document.getElementById('btnsubmit').onclick = function () {
         lettersTypedByWordCreator = input.value;
         // console.log(lettersTypedByWordCreator);
-        arrayOflettersTypedByCreator = lettersTypedByWordCreator.split("");
+        arrayOfLettersTypedByCreator = lettersTypedByWordCreator.split("");
         // console.log(arrayOflettersTypedByCreator);
         // console.log(arrayOflettersTypedByCreator.length);
         generateUnderscore();
 
-        
+
         const content = alphabet.map(letter => `<button type="button" class="letter-button" value="${letter}">${letter}</button>`);
         const div = document.getElementById('buttonGroup');
         const output = document.getElementById('output');
         div.innerHTML += content;
 
-        Array.from(document.getElementsByClassName("letter-button"))
-            .forEach((e) => e.addEventListener('click', () => {
-                output.innerHTML += e.value;
-                letterTyped = e.value;
-                arrayOflettersTypedByCreator.forEach(keyboardAction)
-                // console.log(arrayOflettersTypedByCreator);
-                
-            }));
+        $('.letter-button').click(function () {
+            //console.log('hello world')
+            // $(this).value
+            // const letterClicked = $(this).html();
+            // console.log(letterClicked);
+            // if(arrayOfLettersTypedByCreator.indexOf(letterClicked) > -1){
+            // console.log(arrayOfLettersTypedByCreator);
+            // }
+            // else if (arrayOfLettersTypedByCreator.indexOf(letterClicked) < 0){
+            //     console.log(arrayOfLettersTypedByCreator);
+            // }
+            // if (arrayOfLettersTypedByCreator.includes(letterClicked)) {
+            //     $(this).addClass('success');
+            // }
+            giveLetterAColor($(this));
 
-            
 
-            const keyboardAction = (item, index, arrayOflettersTypedByCreator) => {
-                for (let i = 0; i < arrayOflettersTypedByCreator.length; i++ ){
-              
-                    if (letterTyped === arrayOflettersTypedByCreator[i]){
-                        console.log(letterTyped + " exits");
+        })
 
-                    }
-                }
-               
-                // console.log(item);
-                // console.log(index);
-                // console.log(arrayOflettersTypedByCreator);
-            }
+        // When a letter is clicked,
+        // Check if the letter that was clicked is in arrayOfLettersTypedByCreator
+        // If it is in the array, 
+        // turn the letter green and add it to the dashes on the page
+        // Else 
+        // Turn the letter red and add one to the number of incorrect moves
+
     };
 
-   
-
     let generateUnderscore = () => {
-        for (let i = 0; i < arrayOflettersTypedByCreator.length; i++) {
-            $('.underscores').append('_ ');
+        for (let i = 0; i < arrayOfLettersTypedByCreator.length; i++) {
+            $('.underscores').append(`<div class="underscore">_</div>`);
+
+            //<div class="underscore">_</div>
             // console.log(arrayOflettersTypedByCreator.length);
         }
     }
 
+    //
 
 }
 $onload();
