42ae2b0fc765b12098f6a1767e2e291c697e9dec
Sonia Bastos <Sonia.Bastos@Nology.local>
2020-02-22 18:37:58 +0000

    add keyboard support

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const getEqualLettersIndexes = (item, inputtedWord) => {
+const getEqualLettersIndexes = (letterButton, inputtedWord) => {
     let indexOfEqualLetters = [];
     for (let i = 0; i < inputtedWord.length; i++) {
-        if (item.html() === inputtedWord[i]) {
+        if (letterButton.value === inputtedWord[i]) {
             indexOfEqualLetters.push(i)
         }
     }
@@ -9,20 +9,20 @@ const getEqualLettersIndexes = (item, inputtedWord) => {
 }
 
 const handleLetterSelectionAndRecalculateTriesLeft = (letterButton, inputtedWord, triesLeft) => {
-    if (inputtedWord.includes(letterButton.html())) {
-        letterButton.addClass('success');
+    if (inputtedWord.includes(letterButton.value)) {
+        letterButton.classList.add('success');
         //replace the underscores by letters chosen
         const indexes = getEqualLettersIndexes(letterButton, inputtedWord);
         // console.log(indexes);
         const divs = $('.underscore');
         divs.each((indexOfDiv) => {
             if (indexes.includes(indexOfDiv)) {
-                divs[indexOfDiv].innerHTML = letterButton.html();
+                divs[indexOfDiv].innerHTML = letterButton.value;
             }
         })
         // console.log(divs);
     } else {
-        letterButton.addClass('error');
+        letterButton.classList.add('error');
         triesLeft--;
         $('.lives').text(`You have ${triesLeft} live(s) left`);
     }
@@ -56,22 +56,26 @@ let generateUnderscore = (inputtedWord) => {
 const createKeyboard = () => {
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    const content = alphabet.map(letter => `<button type="button" class="letter-button" value="${letter}">${letter}</button>`);
+    const content = alphabet.map(letter => `
+    <button id="${letter}" type="button" class="letter-button" value="${letter}">
+        ${letter}
+    </button>`);
+
     const div = document.getElementById('buttonGroup');
     div.innerHTML += content.join('');
 }
 
 const $onload = () => {
-
     $('#word').on('keypress', function (event) {
         // console.log(event);
         if (event.key == 'Enter') { //key 13 is the enter key
-            $('#btnsubmit').click()
+            $('#btnsubmit').click();
         }
     });
 
     $('#btnsubmit').one('click', function () {
         $('#btnsubmit').hide();
+        $('#word').hide();
         let triesLeft = 10;  //number of tries left for the user to guess
         let input = document.getElementById('word');
         let inputtedWord = input.value; //Stores the string typed by word creator
@@ -79,27 +83,18 @@ const $onload = () => {
         createKeyboard();
 
         $('.letter-button').on("click", function () {
-            triesLeft = handleLetterSelectionAndRecalculateTriesLeft($(this), inputtedWord, triesLeft);
+            triesLeft = handleLetterSelectionAndRecalculateTriesLeft(this, inputtedWord, triesLeft);
             winnerOrLooser(triesLeft);
         })
+
+        $(document).keypress(function (event) {
+            const pressedLetterButton = $(`#${event.key}`);
+            pressedLetterButton.click();
+        });
 
         $('#reset').click(function () {
             location.reload(true);
         })
     })
-
-
-
-    // const resetBoard = () => {
-    //     maxTries = 10;
-    //     arrayOfLettersTypedByCreator = [];
-    //     console.log(maxTries, arrayOfLettersTypedByCreator);
-    //     $('.underscore').empty();
-    //     $('.underscores').empty();
-    //     $('#word').val("");
-    //     $('.lives').text(`You have ${maxTries} lives left`);
-    //     $('.letter-button').removeClass('success');
-    //     $('.letter-button').removeClass('error');
-
 }
 $onload();
